65d6290d1122a89a2dfb91eb2dbb1df0721e427c
Артём <63098004+eleness@users.noreply.github.com>
2024-09-17 23:37:02 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -26,6 +26,6 @@ const app = createApp(App);
 // Register Framework7 Vue components
 registerComponents(app);
 
-console.log(window.Telegram.WebApp.initData)
+console.log(window.Telegram.WebApp)
 // Mount the app
 app.mount('#app');
