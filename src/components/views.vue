def8551f5682e833c842b7011889ce049e9098a5
Артём <63098004+cratko@users.noreply.github.com>
2024-10-17 00:35:14 +0300

    Update views.vue

--- src/components/views.vue
+++ src/components/views.vue
@@ -44,7 +44,7 @@ export default{
 
 
         const user = useStore('userGetter');  
-        console.log(store.state.userState[0])
+        console.log(store.state.userState)
         console.log(store.state.userState.token)
         console.log(store.state.userState[0].token)
         if (user.value.token) {
